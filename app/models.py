e7b1c67f7757812c4df35543d0959add52493fd0
Jhon Casas <gerencia.full@hotmail.com>
2025-12-29 11:33:46 -0500

    Actualizar what

--- app/models.py
+++ app/models.py
@@ -41,7 +41,6 @@ class User(UserMixin):
                     self._permisos = set()
                     
         return self._permisos
-        
 
     def can(self, permiso):
         return 'acceso_total' in self.permisos or permiso in self.permisos
