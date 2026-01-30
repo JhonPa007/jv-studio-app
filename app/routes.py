a3a9f4a9bda03c7bc53f65f455ce4ad4426405f4
Jhon Casas <gerencia.full@hotmail.com>
2026-01-30 14:31:43 -0500

    correccion factura

--- app/routes.py
+++ app/routes.py
@@ -10109,7 +10109,10 @@ def _generar_y_firmar_xml(venta_id):
     ET.SubElement(invoice, ET.QName(NS_MAP["cbc"], "InvoiceTypeCode"), 
                   listID="0101", 
                   listAgencyName="PE:SUNAT", 
-                  listName="Tipo de Documento"
+                  listName="Tipo de Documento",
+                  listURI="urn:pe:gob:sunat:cpe:see:gem:catalogos:catalogo01",
+                  name="Tipo de Operacion",
+                  listSchemeURI="urn:pe:gob:sunat:cpe:see:gem:catalogos:catalogo51"
                   ).text = tipo_code
     ET.SubElement(invoice, ET.QName(NS_MAP["cbc"], "DocumentCurrencyCode")).text = "PEN"
 
